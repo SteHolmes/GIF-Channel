a8208e3bdf063276a5cdb0a7ab1d550ecd3af0b2
Steve Holmes <steveholmesart@outlook.com>
2019-05-09 17:59:39 +0000

    hide .content-overlay on page transition

--- js/app.js
+++ js/app.js
@@ -45,14 +45,12 @@ $(document).ready(function() {
     var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=kJRLU4rL7DZPj4ioHjxH8WIe7foyFk2u&limit=50";
     
     display.html("").css({"background-image":"none", "height":"auto"});
-    var clearCountdown = $(".countdown").empty();
   
       $.ajax(
       {
         type: 'GET',
         url: url, 
         success: function(res){ 
-                  clearCountdown;
                   res.data.forEach(function(item) {
                     var giphyURL = item.images.fixed_height.url;
                     display.append("<img class='gif' src='" + giphyURL + "'></img>");
@@ -65,12 +63,16 @@ $(document).ready(function() {
   // PAGE TRANSITION //
   function loadTransition() {  
     
+    
+    
     var tl = new TimelineMax();
     
     tl
     .to(".screen-wipe-top", 0.5,{y: "50%", repeat: 1, yoyo: true})
     .to(".screen-wipe-bottom", 0.5,{y: "-50%", repeat: 1, yoyo: true}, 0)
+    .to(".content-overlay", 0.5,{display : 'block', repeat: 1, yoyo: true}, 0)
     .to(".content-overlay", 0.5,{opacity : '1', repeat: 1, yoyo: true}, 0)
+    
     
   }
   
